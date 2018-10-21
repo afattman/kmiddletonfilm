26fd68184d1723ea5d871a5c87b13e7943c0789d
Anthony Fattman <AFattman@1AAuto.com>
2018-10-21 14:42:04 -0400

    More work on front, project, home page

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 // 
-router.get('/frontpage', function(req, res, next) {
-  res.render('frontpage', { title: 'Middleton Film' });
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Middleton Film' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Middleton Film' });
+});
 
 module.exports = router;
